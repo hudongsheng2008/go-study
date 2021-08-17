459a85a625362698c3538685514322020485f36b
hudongsheng <hudongsheng@bytedance.com>
2021-08-17 16:02:18 +0800

    add
    Change-Id: I04596d4b44ebc4800cdc6b8b2839eb473cf135d0

--- go-basic/main.go
+++ go-basic/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -28,4 +29,8 @@ func main()  {
 		})
 	})
 	r.Run(":9090")
+}
+
+func add()  {
+	fmt.Println("ccc")
 }
